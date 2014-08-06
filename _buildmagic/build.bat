84c8d78c044a07fab6ebf3ca762fb3359fbffe98
hairybreeches <dc345@cantab.net>
2014-08-06 16:22:31 +0100

    use gulp for building!

--- _buildmagic/build.bat
+++
@@ -1,2 +0,0 @@
-call jade ..\_jade\pages --out ..\
-call lessc ..\_style\style.less ..\style.css
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    jade = require('gulp-jade'),
+    less = require('gulp-less'),
+    path = require('path'),
+		yargs = require('yargs');
+		
+		
+var args   = yargs.argv;
+
+var targetDir = args.target || './';
+
+gulp.task('templates', function() {
+  return gulp.src('./_jade/pages/*.jade')
+      .pipe(jade())
+      .pipe(gulp.dest(targetDir));
+});
+
+gulp.task('less', function () {
+  gulp.src('./_style/style.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest(targetDir));
+});
+
+gulp.task('content', function () {
+    return gulp.src('content/*')
+      .pipe(gulp.dest(targetDir));
+});
+
+// Default Task
+gulp.task('default', ['templates', 'less', 'content']);
