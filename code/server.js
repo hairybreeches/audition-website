38067ad159565fb149b467a062c4a2010088f2e9
hairybreeches <dc345@cantab.net>
2014-09-24 18:12:14 +0100

    make mailchimp signup work!

--- code/server.js
+++ code/server.js
@@ -1,7 +1,13 @@
 var express = require('express'),
-		fs = require('fs');
-		path = require('path');
+		fs = require('fs'),
+		path = require('path'),
+		mcapi = require('./node_modules/mailchimp-api/mailchimp'),
+		bodyParser = require('body-parser');
+		
 var app = express();
+var mailchimp = new mcapi.Mailchimp('8b4f8984d7258cc1bd38e48050f0c009-us8');
+
+app.use( bodyParser.urlencoded() );
 
 var servePath = path.resolve('./static');
 var port = process.env.PORT || 1337;
@@ -10,6 +16,18 @@ var port = process.env.PORT || 1337;
 app.get('/', function(req,res) {
 	res.sendFile(servePath + '/index.html');		
 });
+
+//mailing list signup
+app.post('/demo-request', function(req, res){
+	mailchimp.lists.subscribe({
+	id: 'b9144c63ee',
+	double_optin: false,
+	email: {
+		email: req.body.EMAIL
+	}
+	});
+	res.send('');
+})
 
 //very dirty way to make sure we serve .html files on their route
 app.use(function(req, res, next) {
